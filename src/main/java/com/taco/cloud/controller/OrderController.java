c828adf2bbbb2efdfb7765e1cf5c36e8d03f3167
Sandipan Ghorai <sandipan.ghorai93@gmail.com>
2022-03-19 14:20:07 +0530

    Taco Design and Order Place

---
+++ src/main/java/com/taco/cloud/controller/OrderController.java
@@ -0,0 +1,30 @@
+package com.taco.cloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.taco.cloud.domain.TacoOrder;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/orders")
+public class OrderController {
+
+	@GetMapping("/current")
+	public String orderForm(Model model) {
+		model.addAttribute("tacoOrder", new TacoOrder());
+		return "orderForm";
+	}
+
+	@PostMapping
+	public String processOrder(TacoOrder order) {
+		log.info("Order submitted: " + order);
+		return "redirect:/";
+	}
+
+}
---
+++ src/main/java/com/taco/cloud/domain/Taco.java
@@ -0,0 +1,13 @@
+package com.taco.cloud.domain;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class Taco {
+	
+	private String name;
+	private List<Ingredient> ingredients;
+	
+}
